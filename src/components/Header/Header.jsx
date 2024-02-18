bb76b92204e62df4dba367d91175da58bae48216
Владимир Ветушкин <vladimirvetuskin@MacBook-Pro-Vladimir.local>
2024-02-18 20:06:45 +0300

    insert logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,6 @@ import classes from "./Header.module.css";
 
 export default function Header() {
     return (
-        <h1 className={classes["heading"]}>Vetus-to-do</h1>
+        <h1 className={classes["heading"]}><img src="/vite.svg" alt="V like Vite"></img>etus-to-do</h1>
     )
 }
